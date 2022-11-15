c3d7a636c5ddb4b47ffe3594de72317e62579a18
Trần Hải Ngọc <ngoctran180199@gmail.com>
2022-11-16 00:46:37 +0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,30 @@ import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
